ef7c87132f789cfcc56ab9db6ed0bf37e141710b
kitto-oby <suhan201552@gmail.com>
2026-04-03 10:00:50 +0800

    修改b站主页与网站地址

--- src/config.ts
+++ src/config.ts
@@ -275,13 +275,13 @@ export const navBarConfig: NavBarConfig = {
 			children: [
 				{
 					name: "GitHub",
-					url: "https://github.com/LyraVoid/Mizuki",
+					url: "https://github.com/kitto-oby",
 					external: true,
 					icon: "fa7-brands:github",
 				},
 				{
 					name: "Bilibili",
-					url: "https://space.bilibili.com/701864046",
+					url: "https://space.bilibili.com/529500992",
 					external: true,
 					icon: "fa7-brands:bilibili",
 				},
